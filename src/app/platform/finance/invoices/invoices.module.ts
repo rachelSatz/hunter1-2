4378a935d8aef40a722d8411e58f1fcf8b4b1efb
Ruth Weiss <ruthw@smarti.co.il>
2018-11-12 15:19:09 +0200

    skdfjlsd

--- src/app/platform/finance/invoices/invoices.module.ts
+++ src/app/platform/finance/invoices/invoices.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InvoicesComponent } from './invoices.component';
-import {RouterModule, Routes} from '@angular/router';
-import {DataTableModule} from 'app/shared/data-table/data-table.module';
+import { RouterModule, Routes } from '@angular/router';
+import { DataTableModule } from 'app/shared/data-table/data-table.module';
+import { BdSelectModule } from 'app/../assets/js/bd-select/bd-select.module';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatDialogModule, MatCheckboxModule,
+  MatSelectModule, MatIconModule, MatAutocompleteModule } from '@angular/material';
+import { DatePickerModule } from '../../../shared/app-date-picker/app-date-picker.module';
+import { PipesModule } from '../../../shared/_pipes/pipes.module';
 
 const routes: Routes = [
   { path: '', component: InvoicesComponent }
@@ -11,8 +17,15 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes),
+    MatFormFieldModule, MatInputModule, MatDialogModule, MatCheckboxModule, MatSelectModule, MatIconModule,
+    MatAutocompleteModule,
+    DatePickerModule,
     DataTableModule,
+    BdSelectModule,
+    PipesModule
+
   ],
   declarations: [InvoicesComponent]
 })
