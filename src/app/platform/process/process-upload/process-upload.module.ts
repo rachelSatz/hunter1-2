d9a2ac0f8bedf1f9f5a4af2d9ce0eddca776dfe4
Shoshi <shoshi@smarti.co.il>
2019-01-03 17:49:18 +0200

    'sa'

--- src/app/platform/process/process-upload/process-upload.module.ts
+++ src/app/platform/process/process-upload/process-upload.module.ts
@@ -9,8 +9,8 @@ import { DetailedFilesComponent } from './detailed-files/detailed-files.componen
 
 const routes: Routes = [
   { path: '', component: ProcessUploadComponent, children: [
-      { path: 'payment', loadChildren: './process-data/process-data.module#ProcessDataModule' },
-      { path: '', loadChildren: './payment/payment.module#PaymentModule' }
+      { path: '', loadChildren: './process-data/process-data.module#ProcessDataModule' },
+      { path: 'payment', loadChildren: './payment/payment.module#PaymentModule' }
       // { path: 'broadcast', loadChildren: './payment/payment.module#PaymentModule' }
     ]}
 ];
