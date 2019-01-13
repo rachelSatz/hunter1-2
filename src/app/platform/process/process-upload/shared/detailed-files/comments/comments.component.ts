c675734c4ba6972b6213599645df14920bfa75c2
Shoshi <shoshi@smarti.co.il>
2019-01-13 17:44:52 +0200

    's'

--- src/app/platform/process/process-upload/shared/detailed-files/comments/comments.component.ts
+++ src/app/platform/process/process-upload/shared/detailed-files/comments/comments.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comments',
-  templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.css']
+  templateUrl: './comments.component.html'
 })
 export class CommentsComponent implements OnInit {
 
