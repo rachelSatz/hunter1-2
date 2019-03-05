3691c1c5605122263c8dcf539258ac3a56716bcf
Ruth Weiss <ruthw@smarti.co.il>
2019-03-05 11:58:06 +0200

    changes

--- src/app/platform/feedback/employees/send-application/send-application.module.ts
+++ src/app/platform/feedback/employees/send-application/send-application.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SendApplicationComponent } from './send-application.component';
-import { MatFormField, MatInputModule, MatOptionModule, MatSelectModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatFormField,
+    MatFormFieldModule,
     MatSelectModule,
     MatOptionModule,
     MatInputModule
