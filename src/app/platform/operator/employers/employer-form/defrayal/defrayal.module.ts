7a179e9667dae73bc73f292b3ab81d54466d7876
Shoshi <shoshi@smarti.co.il>
2019-03-06 15:11:42 +0200

    's'

--- src/app/platform/operator/employers/employer-form/defrayal/defrayal.module.ts
+++ src/app/platform/operator/employers/employer-form/defrayal/defrayal.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefrayalComponent } from './defrayal.component';
 import { RouterModule, Routes } from '@angular/router';
+import {DataTableModule} from '../../../../../shared/data-table/data-table.module';
 
 const routes: Routes = [{ path: '', component: DefrayalComponent }];
 
@@ -9,7 +10,8 @@ const routes: Routes = [{ path: '', component: DefrayalComponent }];
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    DataTableModule
   ],
   declarations: [DefrayalComponent]
 })
