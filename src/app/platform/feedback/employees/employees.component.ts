9a706c62a2e5c8d2be80fe7a4e3f70865a126b25
Shoshi <shoshi@smarti.co.il>
2019-07-21 12:38:31 +0300

    's'

--- src/app/platform/feedback/employees/employees.component.ts
+++ src/app/platform/feedback/employees/employees.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog } from '@angular/material';
 import { Subscription } from 'rxjs';
 
