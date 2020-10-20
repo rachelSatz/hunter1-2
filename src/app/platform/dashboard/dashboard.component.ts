f28502154c31a26779137a4b4a6168012251bc90
maayan malka <maayanm@smarti.co.il>
2020-10-20 10:44:53 +0300

    'maayan'

--- src/app/platform/dashboard/dashboard.component.ts
+++ src/app/platform/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GeneralService} from '../../shared/_services/http/general.service';
+import { DatePipe } from '@angular/common'
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,33 +13,44 @@ export class DashboardComponent implements OnInit {
   projects = [];
   projectId: number;
   timeRange = [{id: 1, name: 'לפי חודש'}, {id: 2, name: 'לפי תקופה'}];
-  timeRangeId: number = 1;
+  timeRangeId: number;
   month: Date;
   fromDate: Date;
   toDate: Date;
   ifByMonth: boolean = true;
-
-  constructor(private GeneralService: GeneralService) {
+  monthStr: string;
+  fromDateStr: string;
+  toDateStr: string;
+  sum_invoices_system: number
+   data: {}
+  constructor(private GeneralService: GeneralService,
+              public datepipe: DatePipe) {
   }
 
   ngOnInit() {
     this.fetchItems();
+
+
   }
 
   fetchItems(): void {
     this.GeneralService.getProjects(1)
-      .then(response => this.projects = response[('1')]);
+      .then(response => {  this.projects = response[('1')];
+                                      this.month = new Date();
+                                      this.projectId = 1;
+                                      this.timeRangeId = 1;
+                                      this.filterData(); });
   }
 
   filterData(): void {
-    console.log(this.projectId);
-    console.log(this.timeRangeId);
-    console.log(this.month);
-    console.log(this.fromDate);
-    console.log(this.toDate);
+    this.monthStr = this.datepipe.transform(this.month, 'yyyy-MM-dd');
+    this.fromDateStr = this.datepipe.transform(this.fromDate, 'yyyy-MM-dd');
+    this.toDateStr = this.datepipe.transform(this.toDate, 'yyyy-MM-dd');
     if (this.timeRangeId === 2)
       this.ifByMonth = false;
-    this.GeneralService.get_financial_data(this.projectId, this.ifByMonth, this.month, this.fromDate, this.toDate)
-      .then(response => console.log(response))
+    this.GeneralService.get_financial_data(this.projectId, this.ifByMonth, this.monthStr, this.fromDateStr, this.toDateStr)
+      .then(response =>{ this.data = response['data'];
+                                    console.log(this.data);
+                                    this.sum_invoices_system = this.data['invoice_system']['green_invoices']['sum']+this.data['invoice_system']['green_invoices_error']} )
   }
 }
